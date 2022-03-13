b134148123659f505766c04ee12898b48abc8106
Alex Baretsky <41ax@mail.ru>
2022-03-13 23:15:27 +0400

    Eslint fixes

--- src/components/Seo/Seo.jsx
+++ src/components/Seo/Seo.jsx
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
@@ -35,35 +28,35 @@ function Seo({ title, description = '', lang = 'en', meta = [] }) {
 
   const metaData = [
     {
-      name: `description`,
+      name: 'description',
       content: metaDescription,
     },
     {
-      property: `og:title`,
+      property: 'og:title',
       content: title,
     },
     {
-      property: `og:description`,
+      property: 'og:description',
       content: metaDescription,
     },
     {
-      property: `og:type`,
-      content: `website`,
+      property: 'og:type',
+      content: 'website',
     },
     {
-      name: `twitter:card`,
-      content: `summary`,
+      name: 'twitter:card',
+      content: 'summary',
     },
     {
-      name: `twitter:creator`,
-      content: site.siteMetadata?.author || ``,
+      name: 'twitter:creator',
+      content: site.siteMetadata?.author || '',
     },
     {
-      name: `twitter:title`,
+      name: 'twitter:title',
       content: title,
     },
     {
-      name: `twitter:description`,
+      name: 'twitter:description',
       content: metaDescription,
     },
   ].concat(meta);
